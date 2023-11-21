9b4fcd95761199c3f3f4f0e675fd443efe28c6c6
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-21 09:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+
+	// return key in sampleObject
+
+	return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+const key = prompt("Enter Key.")
 alert(hasKey(key));
